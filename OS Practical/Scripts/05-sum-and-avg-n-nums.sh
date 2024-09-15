96bc5ef9bd754f0751287fbc1af35d68daaa1da6
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-09-15 19:52:11 +0000

    added script 5

---
+++ OS Practical/Scripts/05-sum-and-avg-n-nums.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Write A Shell Script To Find Sum & Average Of N Numbers.
+
+# getting value of inputs to take from user
+read -p "Enter N: " N
+echo # blank line
+
+total=0 # initializing variable 'total' with 0
+
+# loop to take numbers as input from the user
+for (( i=1; i<=N; i++ ))
+    do
+        read -p "Enter number $i : " num
+        total=$((total+num)) # finding total
+done
+
+# finding the average of all numbers
+avg=$((total/N))
+
+# printing the required output
+echo # blank line
+echo "Sum = $total"
+echo "Average = $avg"
