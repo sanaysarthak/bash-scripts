f296a0212f52eceae05516a4f84ddd3f0fbfdfb4
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-09-26 18:24:37 +0000

    added script 25

--- OS Practical/Scripts/25-swap-two-values.sh
+++ OS Practical/Scripts/25-swap-two-values.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# init (fix errors)
-
 # Write a shell program to swap two values.
 
-read -p "Enter val_1 : " val_1
-read -p "Enter val_2 : " val_2
+read -p "Enter val_1: " val_1
+read -p "Enter val_2: " val_2
 
-echo "Before Swapping:-"
-echo "val_1 : " $val_1
-echo "val_2 : " $val_2
+echo "Before Swapping:"
+echo "val_1: $val_1"
+echo "val_2: $val_2"
 
-temp=$(val_1)
-val_1=$(val_2)
-val_2=$(temp)
+# Swapping values using a temporary variable
+temp=$val_1
+val_1=$val_2
+val_2=$temp
 
-echo "After Swapping:-"
-echo "val_1 : " $val_1
-echo "val_2 : " $val_2
+echo "After Swapping:"
+echo "val_1: $val_1"
+echo "val_2: $val_2"
